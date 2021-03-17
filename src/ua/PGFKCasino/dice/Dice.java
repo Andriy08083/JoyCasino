17bdeb991cd282f7ee6b1dc691dbb72f9e48f95f
swik <80163666+swikeroff@users.noreply.github.com>
2021-03-17 15:26:01 +0200

    Update Dice.java

--- src/ua/PGFKCasino/dice/Dice.java
+++ src/ua/PGFKCasino/dice/Dice.java
@@ -1,52 +1,51 @@
 package ua.PGFKCasino.dice;
 
+import static org.fusesource.jansi.Ansi.Color.*;
+import static org.fusesource.jansi.Ansi.ansi;
+
 import handlers.IOHandler;
 import ua.PGFKCasino.interfaces.ICasinoGame;
+import ua.PGFKCasino.profile.Profile;
 import java.util.Random;
 
-public class Dice extends IOHandler implements ICasinoGame{
-    int bet;
-    int money = 0;
-    int bonus = 100;
 
-    public Dice() {
-        money += bonus;
-        System.out.println("Вітаю. Ви отримали бонус: "+ bonus + " гривень!");
-        startGame();
+public class Dice extends IOHandler implements ICasinoGame{
+    String name;
+    Profile profile;
+    int bet, money; 
+
+    public Dice(Profile pr) {
+        profile = pr;
+        loadGame();
     }
     
     public void spinDice(){
-        int diceone = 1 + new Random().nextInt(6);
-        int dicetwo = 1 + new Random().nextInt(6);
-        int dicethree = 1 + new Random().nextInt(6);
-        System.out.println("Значення кубиків: " + diceone + " | "  + dicetwo + " | "  + dicethree );
-    }
-    
-    public void resultGame(int coef){
-        if(coef == 4 || coef == 3 || coef == 2){
-            money = money + bet * coef;
-            System.out.println("Ваш виграш - " + bet * coef + " гривень!");
-        }else if(coef == 0){
-            System.out.println("Ви програли");
-        }
-        System.out.println("Ваш баланс - " + money + " гривень!");
-    }
-
-    @Override
-    public void startGame() {
-        System.out.println("Ви розпочали гру");
-        
-        System.out.print("Зробіть ставку: ");
-            bet = getInput();
-            money = money - bet;
-        System.out.print("Ставка прийнята.");
-        System.out.print("Введіть значення кубика: ");
-            int dice = getInput();
+        int dice = getInput();
+        int diceone, dicetwo, dicethree;   
+        String diceonecolor, dicetwocolor, dicethreecolor;
             
-        int diceone = 1 + new Random().nextInt(6);
-        int dicetwo = 1 + new Random().nextInt(6);
-        int dicethree = 1 + new Random().nextInt(6);
-        System.out.println("Значення кубиків: " + diceone + " | "  + dicetwo + " | "  + dicethree );
+        diceone = 1 + new Random().nextInt(6);
+        dicetwo = 1 + new Random().nextInt(6);
+        dicethree = 1 + new Random().nextInt(6);
+        
+        if(diceone == dice){
+            diceonecolor = ansi().fg(GREEN).a(diceone).reset().toString();
+        }else {
+            diceonecolor = ansi().fg(RED).a(diceone).reset().toString();
+        }
+        
+        if(dicetwo == dice){
+            dicetwocolor = ansi().fg(GREEN).a(dicetwo).reset().toString();
+        }else {
+            dicetwocolor = ansi().fg(RED).a(dicetwo).reset().toString();
+        }
+        
+        if(dicethree == dice){
+            dicethreecolor = ansi().fg(GREEN).a(dicethree).reset().toString();
+        }else {
+            dicethreecolor = ansi().fg(RED).a(dicethree).reset().toString();
+        }
+        System.out.println("Значення кубиків: " + diceonecolor + " | "  + dicetwocolor + " | "  + dicethreecolor );
         
         if(diceone == dice && dicetwo == dice && dicethree == dice){
             resultGame(4);
@@ -58,6 +57,56 @@ public class Dice extends IOHandler implements ICasinoGame{
             resultGame(0);
         }
     }
+    
+    public void resultGame(int coef){
+        if(coef == 4 || coef == 3 || coef == 2){
+            money = money + bet * coef;
+            System.out.println("Ваш виграш - " + bet * coef);
+        }else if(coef == 0){
+            System.out.println("Ви програли.");
+        }
+        System.out.println("Ваш баланс - " + money);
+    }
+
+    @Override
+    public void startGame() {
+        System.out.println("Ви розпочали гру!");
+        System.out.println("Ваш баланс: " + money);
+        
+        if (money <= 0) {
+            System.out.println("Ви не можете грати, у вас нульовий баланс.");
+            stopGame();
+            return;
+        }
+        
+        System.out.print("Зробіть ставку: ");
+            bet = getInput();
+            money = money - bet;
+        System.out.println("Ставка прийнята!");
+        System.out.print("Введіть значення кубика: ");
+        spinDice();
+        
+        saveGame();
+        
+        System.out.println("Ставимо ще?");
+        System.out.println("0. Нi, 1. Так");
+        isGameContinue();
+    }
+    
+    public void isGameContinue(){
+        switch (handleInput().trim()) {
+            case "1":
+                startGame();
+                break;
+            case "2":
+                stopGame();
+                break;
+            default:
+                System.out.println("Незрозумiла команда. Введіть ще раз.");
+                isGameContinue();
+                break;
+        }
+    }
 
     public Integer getInput() {
         try {
@@ -65,7 +114,7 @@ public class Dice extends IOHandler implements ICasinoGame{
             if (money >= input && input > 0) {
                 return input;
             } else
-                throw new Exception("У вас недостатньо коштiв");
+                throw new Exception("У вас недостатньо коштiв.");
         }
         catch (Exception e) {
             System.out.println(e.getMessage());
@@ -75,17 +124,30 @@ public class Dice extends IOHandler implements ICasinoGame{
     
     @Override
     public void stopGame() {
-
+        System.out.println("Гру завершено.");
+        saveGame();
     }
 
     @Override
     public void saveGame() {
-
+        if (name != null) {
+            writeJSON("profiles/" + name + ".json", name, money);
+            System.out.println("Профiль успiшно збережено.");
+        }
+        else
+            System.out.println("Неможливо зберегти профiль.");
     }
 
     @Override
     public void loadGame() {
-
+        try {
+            name = profile.getName();
+            money = Integer.parseInt(profile.getBalance());
+            startGame();
+        }
+        catch (Exception e) {
+            stopGame();
+        }
     }
 
     @Override
